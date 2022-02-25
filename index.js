e2887ecf4831195e8c5098a5d1103fd4c30005db
arjorb <arjorb@gmail.com>
2022-02-25 16:54:37 +0200

    Building the UI to look exactly as card table at casino and create a function of startGame button

---
+++ index.js
@@ -0,0 +1,23 @@
+let firstCard = 11;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = '';
+
+const startGame = () =>{
+    if(sum <= 20){
+        message = "Do you want to draw a new card? 😊";
+        
+    }
+    else if(sum === 21){
+        message = "Wohoo! You have got Blackjack! 🍾🍻"
+        hasBlackJack = true;
+    }else{
+        message = "You're out of the game! 😭"
+        isAlive = false;
+    }
+    
+    console.log(message)
+}
+
