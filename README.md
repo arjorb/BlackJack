49f45534364e656ee5fcf4aebe0ce32f9c7b5056
arjorb <arjorb@gmail.com>
2022-02-26 10:10:53 +0200

    updating readme note

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BlackJack
 This is one of the iconic casino game! build it using javascript as a core languge
+
+Things that I cover during the implementation of this project
+
+1. arrays
+2. objects
+3. boaleans
+4. if else statement
+5. comparison operator
+6. logical operators
+7. for loops
+8. The Math object
+9. return statement
--- index.js
+++ index.js
@@ -9,6 +9,14 @@ let messageEl = document.getElementById('message-el');
 let sumEl = document.getElementById('sum-el');
 let cardEl  =document.getElementById('card-el');
 
+let player = {
+    name :"John",
+    chips : 150
+}
+
+let playerEl = document.getElementById('player-el');
+playerEl.textContent = `${player.name}: $${player.chips}`;
+
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random() * 13)+1;
     if(randomNumber === 1){
